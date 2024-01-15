ebbb2bd20ccb13fc528356c0438df1b733d2a47d
jeet kanodia <jeetkanodia@gmail.com>
2024-01-16 00:04:00 +0530

    Add HomeCard component and update content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,46 @@
 import React from "react";
+import HomeCard from "../components/HomeCard";
+import about from "../../public/about.jpg";
+import events from "../../public/events.jpg";
+import team from "../../public/team.jpg";
 
 const Home = () => {
+  const content = [
+    {
+      title: "Events",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, hic voluptatibus! Aut, sunt laborum ipsum asperiores, explicabo dolores magni neque necessitatibus quis ut tempora repellat modi. Nobis velit nemo itaque.",
+      link: "events",
+      src: events,
+    },
+    {
+      title: "About",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, hic voluptatibus! Aut, sunt laborum ipsum asperiores, explicabo dolores magni neque necessitatibus quis ut tempora repellat modi. Nobis velit nemo itaque.",
+      link: "about",
+      src: about,
+    },
+
+    {
+      title: "Team",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, hic voluptatibus! Aut, sunt laborum ipsum asperiores, explicabo dolores magni neque necessitatibus quis ut tempora repellat modi. Nobis velit nemo itaque.",
+      link: "team",
+      src: team,
+    },
+  ];
   return (
-    <div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
+    <div className="flex flex-col justify-center items-center">
+      {content.map((item, index) => (
+        <HomeCard
+          key={index}
+          index={index}
+          title={item.title}
+          description={item.description}
+          link={item.link}
+          src={item.src}
+        />
+      ))}
     </div>
   );
 };
